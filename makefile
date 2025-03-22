e3457805728986c51b2528bfcd278ff23ca0dc55
Guangning Zhang <gzha0824@uni.sydney.edu.au>
2025-03-22 12:49:41 +1100

    Completed the wav_load and wav_save functionalities. Now the program can read/write WAV files. Makefile is scripted to compile and generate the binary file.

--- makefile
+++ makefile
@@ -1,1 +1,8 @@
 #TODO
+CFLAGS = -fsanitize=address
+
+sound_seg: sound_seg.c
+	${CC} -o bin/sound_seg sound_seg.c ${CFLAGS}
+
+clean:
+	rm bin/*
--- sound_seg.c
+++ sound_seg.c
@@ -5,19 +5,106 @@
 #include <string.h>
 #include <stdio.h>
 
+#define BUFFER_LEN 128
+#define WAV_HEADER_OFFSET 44
+
 
 // This represents the track.
 struct sound_seg {
     //TODO
 };
 
+typedef struct {
+
+    uint32_t chunkID;
+    uint32_t chunkSize;
+    uint32_t format;
+    uint32_t subChunk1ID;
+    uint32_t subChunk1Size;
+    uint16_t audioFormat;
+    uint16_t numChannels;
+    uint32_t sampleRate;
+    uint32_t byteRate;
+    uint16_t blockAlign;
+    uint16_t bitsPerSample;
+    uint32_t subChunk2ID;
+    uint32_t subChunk2Size;
+
+} WAVHeader;
+
 // Load a WAV file into buffer
-void wav_load(const char* filename, int16_t* dest){
-    return;
+int wav_load(const char* filename, int16_t** dest){
+
+    // Read the binary information from the file.
+    FILE *myWAV = fopen(filename, "rb");
+    // Process the exception
+    if(!myWAV){
+        perror("Open file failed.\n");
+        return 0;
+    }
+
+    // Pass the header.
+    fseek(myWAV, WAV_HEADER_OFFSET-4, SEEK_SET);
+    // 4 Bytes unsined integer indicates the size of data.
+    uint32_t dataSize = 0;
+    fread(&dataSize, sizeof(uint32_t), 1, myWAV);
+    printf("The size of the data: %d\n", dataSize);
+    *dest = malloc(dataSize);
+    fread(*dest, sizeof(uint16_t), dataSize*2, myWAV);
+
+
+    fclose(myWAV);
+
+    return 1;
+}
+
+
+
+// Given an unsigned integer with big endian, convert it to little endian form.
+uint16_t convert_to_little_16(uint16_t big){
+    // For 0xABCD, OR(0xCD00, 0x00AB)
+    return ((big >> 8) | (big << 8));
+}
+
+uint32_t convert_to_little_32(uint32_t big){
+    // For 0xABCDEFGH, OR(0xGH000000, 0x00EF0000, 0x0000CD00, 0x000000AB)
+    return ((big & (0x000000FF)) << 24 | (big & (0x0000FF00)) << 8 | (big & (0x00FF0000)) >> 8 | (big & (0xFF000000)) >> 24);
 }
 
 // Create/write a WAV file from buffer
 void wav_save(const char* fname, int16_t* src, size_t len){
+    // Define a WAV header.
+    WAVHeader myHeader;
+    myHeader.chunkID = 0x52494646;
+    myHeader.chunkSize = convert_to_little_32(36 + len);
+    myHeader.format = 0x57415645;
+    myHeader.subChunk1ID = 0x666d7420;
+    myHeader.subChunk1Size = convert_to_little_32(16);
+    myHeader.audioFormat = convert_to_little_16(1);
+    myHeader.numChannels = convert_to_little_16(1);
+    myHeader.sampleRate = convert_to_little_32(8000);
+    myHeader.byteRate = convert_to_little_32(8000 * 1 * 16 / 8);
+    myHeader.blockAlign = convert_to_little_16(1 * 16 / 8);
+    myHeader.bitsPerSample = convert_to_little_16(16);
+    myHeader.subChunk2ID = 0x64617461;
+    myHeader.subChunk2Size = convert_to_little_32(len);
+
+    // Define the file.
+    FILE *myWAV = fopen(fname, "wb");
+    // Write the header into the file, with the size of the header, once, by bit.
+    fwrite(&myHeader, sizeof(WAVHeader), 1, myWAV);
+
+    // Then, write the data into the file.
+    // There are |len| uint16_t to write.
+    for(int i = 0; i < len; i++){
+
+    }
+
+
+    fclose(myWAV);
+
+
+
     return;
 }
 
@@ -61,4 +148,27 @@ void tr_insert(struct sound_seg* src_track,
             struct sound_seg* dest_track,
             size_t destpos, size_t srcpos, size_t len) {
     return;
+}
+
+// void main(){
+
+//     int16_t *dest;
+
+//     if(wav_load("./Ouch-6.wav", &dest) == 1){
+//         printf("Complete load.\n");
+//     }
+
+
+//     if(dest != NULL){
+//         free(dest);
+//     }
+    
+
+
+// }
+
+
+void main(){
+    printf("%04X\n", convert_to_little_32(36));
+
 }
