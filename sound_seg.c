5dcbde197d9df032fa1414a078c1947de8a511b7
Guangning Zhang <gzha0824@uni.sydney.edu.au>
2025-03-23 00:14:20 +1100

    Complete the struct Track, initialization and destroy function.

--- sound_seg.c
+++ sound_seg.c
@@ -209,37 +209,37 @@ void tr_destroy(struct sound_seg* obj) {
     return;
 }
 
-// Return the length of the segment
-size_t tr_length(struct sound_seg* seg) {
-    return (size_t)-1;
-}
+// // Return the length of the segment
+// size_t tr_length(struct sound_seg* seg) {
+//     return (size_t)-1;
+// }
 
-// Read len elements from position pos into dest
-void tr_read(struct sound_seg* track, int16_t* dest, size_t pos, size_t len) {
-    return;
-}
+// // Read len elements from position pos into dest
+// void tr_read(struct sound_seg* track, int16_t* dest, size_t pos, size_t len) {
+//     return;
+// }
 
-// Write len elements from src into position pos
-void tr_write(struct sound_seg* track, int16_t* src, size_t pos, size_t len) {
-    return;
-}
+// // Write len elements from src into position pos
+// void tr_write(struct sound_seg* track, int16_t* src, size_t pos, size_t len) {
+//     return;
+// }
 
-// Delete a range of elements from the track
-bool tr_delete_range(struct sound_seg* track, size_t pos, size_t len) {
-    return true;
-}
+// // Delete a range of elements from the track
+// bool tr_delete_range(struct sound_seg* track, size_t pos, size_t len) {
+//     return true;
+// }
 
-// Returns a string containing <start>,<end> ad pairs in target
-char* tr_identify(struct sound_seg* target, struct sound_seg* ad){
-    return NULL;
-}
+// // Returns a string containing <start>,<end> ad pairs in target
+// char* tr_identify(struct sound_seg* target, struct sound_seg* ad){
+//     return NULL;
+// }
 
-// Insert a portion of src_track into dest_track at position destpos
-void tr_insert(struct sound_seg* src_track,
-            struct sound_seg* dest_track,
-            size_t destpos, size_t srcpos, size_t len) {
-    return;
-}
+// // Insert a portion of src_track into dest_track at position destpos
+// void tr_insert(struct sound_seg* src_track,
+//             struct sound_seg* dest_track,
+//             size_t destpos, size_t srcpos, size_t len) {
+//     return;
+// }
 
 void main(){
 
